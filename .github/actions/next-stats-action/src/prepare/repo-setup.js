a0868a6924a117801c08bc4e3e7222a8d0995f46
Benjamin Woodruff <benjamin.woodruff@vercel.com>
2026-04-09 13:21:44 -0700

    CI: Remove dead yarn support from next-stats-action

--- .github/actions/next-stats-action/src/prepare/repo-setup.js
+++ .github/actions/next-stats-action/src/prepare/repo-setup.js
@@ -216,9 +216,9 @@ module.exports = (actionInfo) => {
                     if (packageName === '@next/swc') {
                       // next-swc uses a gitignore to prevent the committing of native builds but it doesn't
                       // use files in package.json because it publishes to individual packages based on architecture.
-                      // When we used yarn to pack these packages the gitignore was ignored so the native builds were packed
-                      // however npm does respect gitignore when packing so we need to remove it in this specific case
+                      // pnpm respects gitignore when packing so we need to remove it in this specific case
                       // to ensure the native builds are packed for use in gh actions and related scripts
+                      // pnpm uses https://www.npmjs.com/package/npm-packlist
 
                       const nativeGitignorePath = path.join(
                         pkgPath,
